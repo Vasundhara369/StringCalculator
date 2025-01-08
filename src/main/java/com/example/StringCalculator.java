71e472489c2fa21dccca543153ab0342b8046fa9
Vasundhara <vasundhara99myakala@gmail.com>
2025-01-08 18:26:30 +0530

    Green: Custom delimiter test now passes

--- src/main/java/com/example/StringCalculator.java
+++ src/main/java/com/example/StringCalculator.java
@@ -1,12 +1,23 @@
 package com.example;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
-        return Arrays.stream(numbers.split(",|\n")).mapToInt(Integer::parseInt).sum();
+
+        String delimiter = ",|\n";
+        if (numbers.startsWith("//")) {
+            Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(numbers);
+            if (matcher.find()) {
+                delimiter = matcher.group(1);
+                numbers = matcher.group(2);
+            }
+        }
+        return Arrays.stream(numbers.split(delimiter)).mapToInt(Integer::parseInt).sum();
     }
 }
